c8a75cbe6e28f5da4f205c191ef15737a7b4126a
Imam Pratama Setiady <imampratamasetiady11@gmail.com>
2024-07-08 09:22:58 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function saveData() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    const data = {
+        name: name,
+        email: email,
+        message: message
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const blob = new Blob([jsonData], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'formData.json';
+    a.click();
+    URL.revokeObjectURL(url);
+}
